133e7cb07e2fc8d46b229d2d8555b7b65700ee43
manoj <manoj.sarwate@ssiedu.in>
2021-02-02 16:57:25 +0530

    02-02-21

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+
+public class Test {
+
+    
+    public static void main(String  args[]) {
+        System.out.println("Hello from main method of Test...");
+    }
+
+    public void show() {
+        System.out.println("Hello from show method");
+    }
+
+    public static void disp() {
+        System.out.println("Hello from  disp method");
+    }
+
+}
