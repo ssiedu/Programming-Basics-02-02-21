a844d0587315162cbd23c490c8ed250ab582e30e
manoj <manojs121@rediffmail.com>
2021-02-04 17:00:51 +0530

    04-02-21

---
+++ src/DTypeExample.java
@@ -0,0 +1,15 @@
+
+public class DTypeExample {
+
+    public static void main(String[] args) {
+
+        System.out.println(Double.SIZE);
+        System.out.println(Double.MIN_VALUE);    
+        System.out.println(Double.MAX_VALUE);
+        /*
+        System.out.println(Byte.SIZE);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+        */
+    }
+}
