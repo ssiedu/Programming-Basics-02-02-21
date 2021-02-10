190de827994df36a6bff8a6957916d58aa76c6e4
manoj <manojs121@rediffmail.com>
2021-02-10 16:58:41 +0530

    10-02-21

--- src/LabellingExample.java
+++ src/LabellingExample.java
@@ -3,10 +3,15 @@ public class LabellingExample {
 
     public static void main(String[] args) {
 
-        for(int i=1; i<=5; i++){
-            for(int j=1; j<=3; j++){
+        x:for(int i=1; i<=5; i++){
+            
+            y:for(int j=1; j<=5; j++){
+                
+                if(j==3) continue x;   
+                
                 System.out.println(j);
             }
+            System.out.println("#######");
         }
         
         
