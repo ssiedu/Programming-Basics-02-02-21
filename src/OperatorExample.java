a9644ed3971fdf5fa433ef2e9be5c72090d56963
manoj <manojs121@rediffmail.com>
2021-02-11 16:57:46 +0530

    11-02-21

---
+++ src/OperatorExample.java
@@ -0,0 +1,13 @@
+
+public class OperatorExample {
+
+    public static void main(String[] args) {
+
+        System.out.println(10+20);
+        System.out.println("ab"+"cd");
+        System.out.println("xy"+10);
+        System.out.println("xy"+true);
+        System.out.println("A"+"B");
+        System.out.println('A'+'B');
+    }
+}
